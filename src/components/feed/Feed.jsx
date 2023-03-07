c58a59728e5b578c5f80ddb51d191ed06ecf73d2
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-07 22:03:45 +0530

    adding rateLimit error component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useEffect } from "react";
 // react-router-dom
 import { Navigate } from "react-router-dom";
 
-import HelmetConsumer from "../../shared/helmetConsumer"
+import HelmetConsumer from "../../shared/helmetConsumer";
 
 // context
 import { Context } from "../../context/contextApi";
@@ -12,6 +12,7 @@ import { UserContext } from "../../context/userContext";
 // components
 import LeftNav from "../navbar/leftNav/LeftNav";
 import VideoCard from "../videoDetails/VideoCard";
+import RateLimitError from "../error/RateLimitError";
 
 /*********************************************
  *
@@ -34,21 +35,28 @@ const Feed = () => {
 
   return (
     <>
-    <HelmetConsumer pageTitle={"Home | YouTube app"}/>
-    <div className="flex flex-row h-[calc(100%-56px)]">
-      <LeftNav />
-      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
-          {!loading &&
-            searchResults.map((item) => {
-              if (item.type !== "video") return false;
-              return (
-                <VideoCard key={item?.video?.videoId} video={item?.video} />
-              );
-            })}
+      <HelmetConsumer pageTitle={"Home | YouTube app"} />
+      <div className="flex flex-row h-[calc(100%-56px)]">
+        <div className="hidden md:block">
+          <LeftNav />
+        </div>
+        <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+            {!loading ? (
+              searchResults.map((item) => {
+                if (item.type !== "video") return false;
+                return (
+                  <VideoCard key={item?.video?.videoId} video={item?.video} />
+                );
+              })
+            ) : (
+              <>
+                <RateLimitError />
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
